59ab590954e4360dd89569fcf5d38b9d151999f0
borisbolsh <bolsh.bo@yandex.ru>
2022-01-04 08:22:20 +0300

    first commit

---
+++ Themoviedb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Themoviedb
+//
+//  Created by Boris Bolshakov on 4.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+    }
+
+
+}
+
